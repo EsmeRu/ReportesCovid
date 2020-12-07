19a216e3f8af569136de579949a6297fd7930379
Christian Salas <ch230700@gmail.com>
2020-12-06 18:20:38 -0700

    new DBScript

--- scriptProyectoBD_Web.sql
+++ scriptProyectoBD_Web.sql
@@ -4,27 +4,21 @@ USE ReportesCOVID;
 
 CREATE TABLE IF NOT EXISTS Clientes(
 	idCliente INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    PrimerNombre VARCHAR(30) NOT NULL,
-    SegundoNombre VARCHAR(30),
-    PrimerApellido VARCHAR(30) NOT NULL,
-    SegundoApellido VARCHAR(30),
+    Nombre VARCHAR(100) NOT NULL,
     Email VARCHAR(60) NOT NULL UNIQUE,
     Contraseña BLOB NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Administradores(
 	idAdministrador INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    PrimerNombre VARCHAR(30) NOT NULL,
-    SegundoNombre VARCHAR(30),
-    PrimerApellido VARCHAR(30) NOT NULL,
-    SegundoApellido VARCHAR(30),
+    Nombre VARCHAR(100) NOT NULL,
     Email VARCHAR(60) NOT NULL UNIQUE,
     Contraseña BLOB NOT NULL
 );
 
-INSERT INTO Administradores VALUES (0,'Esmeralda', NULL ,'Rubin','Fausto','rubin.esmef@gmail.com',AES_ENCRYPT('pass123','esme'));
-INSERT INTO Administradores VALUES (0,'Christian', 'Alejandro','Salas','Inzunza','chsa_18@alu.uabcs.mx',AES_ENCRYPT('contra123','chris'));
-INSERT INTO Administradores VALUES (0,'Jesus', 'Antonio','Zuñiga','Arce','	jzuniga@uabcs.mx',AES_ENCRYPT('ponganos10porfa','profe'));
+INSERT INTO Administradores VALUES (0,'Esmeralda Rubin Fausto','rubin.esmef@gmail.com',AES_ENCRYPT('pass123','esme'));
+INSERT INTO Administradores VALUES (0,'Christian Alejandro Salas Inzunza','chsa_18@alu.uabcs.mx',AES_ENCRYPT('contra123','chris'));
+INSERT INTO Administradores VALUES (0,'Jesus Antonio Zuñiga Arce','	jzuniga@uabcs.mx',AES_ENCRYPT('ponganos10porfa','profe'));
 
 CREATE TABLE IF NOT EXISTS Municipios(
 	idMunicipio INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -67,7 +61,7 @@ INSERT INTO Ciudades VALUES(0,'Cabo San Lucas',5);
 CREATE TABLE IF NOT EXISTS Reportes(
 	idReporte INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     idCliente INT NOT NULL,
-    NombreCliente VARCHAR(120) NOT NULL,
+    NombreCliente VARCHAR(100) NOT NULL,
     idMunicipio INT NOT NULL,
     NombreMunicipio VARCHAR(30) NOT NULL,
     idCiudad INT NOT NULL,
@@ -81,18 +75,15 @@ CREATE TABLE IF NOT EXISTS Reportes(
 );
 
 DELIMITER $$
-CREATE PROCEDURE RegistrarCliente(IN paramPrimerNombre VARCHAR(30), IN paramSegundoNombre VARCHAR(30), IN paramPrimerApellido VARCHAR(30), 
-								  IN paramSegundoApellido VARCHAR(30), IN paramEmail VARCHAR(60), IN paramContraseña VARCHAR(20))
+CREATE PROCEDURE RegistrarCliente(IN paramNombre VARCHAR(100), IN paramEmail VARCHAR(60), IN paramContraseña VARCHAR(20))
 BEGIN
-	IF paramPrimerNombre IS NOT NULL THEN
-		IF paramPrimerApellido IS NOT NULL THEN
-			IF paramEmail IS NOT NULL THEN
-				IF paramContraseña IS NOT NULL THEN
-					START TRANSACTION;
-						INSERT INTO Clientes VALUES (0,paramPrimerNombre,paramSegundoNombre,paramPrimerApellido,paramSegundoApellido,paramEmail,AES_ENCRYPT(paramContraseña,'cliente'));
-					COMMIT;
-                END IF;
-            END IF;
+	IF paramNombre IS NOT NULL THEN
+		IF paramEmail IS NOT NULL THEN
+			IF paramContraseña IS NOT NULL THEN
+				START TRANSACTION;
+					INSERT INTO Clientes VALUES (0,paramNombre,paramEmail,AES_ENCRYPT(paramContraseña,'cliente'));
+				COMMIT;
+			END IF;
         END IF;
     END IF;
 END $$
@@ -100,29 +91,23 @@ END $$
 DELIMITER %%
 CREATE TRIGGER Before_Update_Clientes BEFORE UPDATE ON Clientes FOR EACH ROW
 BEGIN
-	IF NEW.PrimerNombre IS NULL THEN
-		SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = 'El primer nombre no puede quedar vacío';
+	IF NEW.Nombre IS NULL THEN
+		SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = 'El nombre no puede quedar vacío';
     ELSE
-		IF NEW.PrimerApellido IS NULL THEN
-			SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = 'El primer apellido no puede quedar vacío';
-        ELSE
-			IF NEW.Email IS NULL THEN
-				SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = 'El correo no puede quedar vacío';
-            ELSE
-				IF NEW.Contraseña IS NULL THEN
-					SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = 'La contraseña no puede quedar vacía';               
-                END IF;
-            END IF;
-        END IF;
+		IF NEW.Email IS NULL THEN
+			SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = 'El correo no puede quedar vacío';
+		ELSE
+			IF NEW.Contraseña IS NULL THEN
+				SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = 'La contraseña no puede quedar vacía';               
+			END IF;
+		END IF;
     END IF;
 END %%
 
 DELIMITER &&
 CREATE PROCEDURE NuevoReporte(IN paramidCliente INT, paramidMunicipio INT, IN paramidCiudad INT, IN paramDireccion VARCHAR(100))
 BEGIN
-	DECLARE varNombreCliente VARCHAR(120);
-    DECLARE varSegundoNombre VARCHAR(30);
-    DECLARE varSegundoApellido VARCHAR(30);
+	DECLARE varNombreCliente VARCHAR(100);
     DECLARE varNombreMunicipio VARCHAR(30);
     DECLARE varNombreCiudad VARCHAR(30);
 	IF paramidCliente IS NOT NULL THEN
@@ -130,17 +115,9 @@ BEGIN
 			IF paramidCiudad IS NOT NULL THEN
 				IF paramDireccion IS NOT NULL THEN 
 					START TRANSACTION;
-						SELECT SegundoNombre INTO varSegundoNombre FROM Clientes WHERE idCliente = paramidCliente;
-                        SELECT SegundoApellido INTO varSegundoApellido FROM Clientes WHERE idCliente = paramidCliente;
                         SELECT Nombre INTO varNombreMunicipio FROM Municipios WHERE idMunicipio = paramidMunicipio;
                         SELECT Nombre INTO varNombreCiudad FROM Ciudades WHERE idCiudad = paramidCiudad;
-                        IF varSegundoNombre IS NULL THEN
-							SET varSegundoNombre = '';
-                        END IF;
-                        IF varSegundoApellido IS NULL THEN
-							SET varSegundoApellido = '';
-						END IF;
-						SET varNombreCliente = CONCAT((SELECT PrimerNombre FROM Clientes WHERE idCliente = paramidCliente),(varSegundoNombre),(SELECT PrimerApellido FROM Clientes WHERE idCliente = paramidCliente),(varSegundoApellido));
+						SELECT Nombre INTO varNombreCliente FROM Clientes WHERE idCliente = paramidCliente;
 						INSERT INTO Reportes VALUES(0,paramidCliente,varNombreCliente,paramidMunicipio,varNombreMunicipio,paramidCiudad,varNombreCiudad,paramDireccion,CURDATE(),'Pendiente');
                     COMMIT;
                 END IF;
@@ -148,3 +125,6 @@ BEGIN
         END IF;
     END IF;
 END &&
+
+SELECT * FROM Clientes;
+SELECT Nombre, Email,AES_DECRYPT(Contraseña, 'cliente') AS Contraseña FROM Clientes;
