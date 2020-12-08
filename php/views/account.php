2ece74abcbfa323367d3e66fde9a4a9c2b83a8c3
Christian Salas <ch230700@gmail.com>
2020-12-08 00:44:50 -0700

    actualizar datos del usuario

--- php/views/account.php
+++ php/views/account.php
@@ -1,5 +1,19 @@
 <?php
-require_once '../includes/head.php';
+    require_once '../databaseconect.php';
+    require_once '../includes/head.php';
+    $action = $_GET['action'];
+
+    if($_SESSION['Type'] === 'Administrador'){
+        $sql = "SELECT * FROM Administradores WHERE idAdministrador = ?";
+    } else {
+        $sql = "SELECT * FROM Clientes WHERE idCliente = ?";
+    }
+
+    $sqlstatement = $connection->prepare($sql);
+    $sqlstatement->bind_param('i',$_SESSION['Id']);
+    $sqlstatement->execute();
+
+    $results = $sqlstatement->get_result()->fetch_assoc();
 ?>
 </head>
 <body>
@@ -7,24 +21,37 @@ require_once '../includes/head.php';
 
     
     
-    <main class="detail-report-container" style="margin-bottom: 3rem;">
-        <form action="" class="flex-container detail-report">
-
-            <div class="flex-container jc-c" style="width: 100%; margin-top: 3rem;">
+    <main class="detail-report-container" style="margin-bottom: 5rem;">
+        <div class="flex-container jc-c titleButton" style="width: 100%; margin-top: 3rem;">
                 <h2>DETALLES<strong>-CUENTA</strong></h2>
-                    <button id="addBtn" class="btn btn-primary">
-                    <i class="fas fa-edit"></i> Editar Cuenta</button>
+                    <?php if($_SESSION['Type'] === 'Cliente'): ?>
+                        <a href="./account.php?action=editar">
+                            <button id="editBtn" class="btn btn-primary">
+                            <i class="fas fa-edit"></i> Editar Cuenta</button>
+                        </a>
+                    <?php endif; ?>
             </div>
 
+        <form action="./update.php" method="post" class="flex-container detail-report">           
+
             <label for="name">Nombre</label>
-            <input type="text" id="name" required>
+            <input type="text" id="name" name="name" value="<?php echo $results['Nombre']; ?>" required>
 
             <label for="email">Correo Electrónico:</label>
-            <input type="email" id="email" required>
+            <input type="email" id="email" name="email" value="<?php echo $results['Email']; ?>" required>
 
             <label for="password">Contraseña:</label>
-            <input type="password" id="password" required>
+            <input type="password" id="password" name="password" value="<?php echo $results['Contraseña']; ?>" required>
+
+            <button type="submit" id="confirmBtn" class="btn btn-primary actualizar-Btn">Confirmar Cambio</button>
         </form>
     </main>
-
+    <script src="../../js/user-ctrl.js"></script>
+    <?php 
+        echo "
+        <script>
+          setDisabled('".$action."')
+        </script>
+        ";
+    ?>
     <?php require_once '../includes/footer.php';?>
